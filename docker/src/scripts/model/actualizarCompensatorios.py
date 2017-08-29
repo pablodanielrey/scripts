b269ce2bed4e9ed4eb65403729676bf693df7f9e
Iván <ivancas84@gmail.com>
2017-08-29 14:02:08 -0300

    actualizar scripts

--- docker/src/scripts/model/actualizarCompensatorios.py
+++ docker/src/scripts/model/actualizarCompensatorios.py
@@ -8,8 +8,7 @@ import csv
 
 if __name__ == '__main__':
 
-
-    
+    fileName = sys.argv[1]
     
     host = os.environ['DB_HOST']
     dbname = os.environ['DB_NAME']
@@ -20,22 +19,36 @@ if __name__ == '__main__':
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         try:
         
-          with open('./scripts/model/compensatorios.csv') as csvfile:
+          with open("./csv/{}".format(fileName)) as csvfile:
               spamreader = csv.reader(csvfile, delimiter=',')
+              
+              errors = []
               for row in spamreader:
                   nombre = row[0]
                   numero_documento = row[1]
-                  compensatorios = row[2]
+                  stock = int(row[2])
 
-                  consulta = cur.execute("SELECT id FROM profile.users WHERE dni = %s", (numero_documento,))
+                  cur.execute("SELECT id FROM profile.users WHERE dni = %s", (numero_documento,))
                   user = cur.fetchone()
+                  
+                  if not user:
+                      errors.push("No existe el usuario {}".format(numero_documento))
+                  
+                  cur.execute("SELECT user_id, stock FROM assistance.justification_compensatory_stock WHERE user_id = %s;", (user["id"],))
+                  compensatorio = cur.fetchone()
+                
 
+                  if not compensatorio:
+                      logging.info("insertar stock de {}. Nuevo stock: {}".format(numero_documento, stock))
+                      cur.execute("INSERT INTO assistance.justification_compensatory_stock (user_id, stock) VALUES (%s, %s)", (user["id"], stock))
+                  else:
+                      nuevo_stock = int(compensatorio["stock"]) + stock
+                      logging.info("actualizar stock de {}. Stock anterior: {}. Nuevo stock: {}".format(numero_documento, compensatorio["stock"], nuevo_stock))
+                      cur.execute("UPDATE assistance.justification_compensatory_stock SET stock = %s WHERE user_id = %s;", (nuevo_stock, user["id"]))
+                  
 
-                                      
-                  cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-        
+                  conn.rollback()
 
-            #return cur.fetchall()
         finally:
           cur.close()
 
