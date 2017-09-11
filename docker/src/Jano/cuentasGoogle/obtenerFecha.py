363033053f43c085709fcee8708f94000df34d41
Jano <oportoalejandroa@gmail.com>
2017-09-11 14:38:55 -0300

    modificacion del script para que exporte las direcciones fuera del for

--- docker/src/Jano/cuentasGoogle/obtenerFecha.py
+++ docker/src/Jano/cuentasGoogle/obtenerFecha.py
@@ -16,24 +16,40 @@ cuentasGoogle = open("cuentasGoogle.txt", "w")
 
 cantUsuarios=0
 
-for linea in leerArchivoUA:
-    dni=linea[0].replace('@econo.unlp.edu.ar','')
-    if int(linea[1]) > 20170600:
-        ultimoAcceso.add(dni)
-
-for linea in leerU:
-    dni=linea[0]
-    correo=linea[1]
-    if dni in ultimoAcceso:
-        direcciones=usuariosGoogle.get(dni, [])
-        if 'econo.unlp.edu.ar' in correo:
-            direcciones.append(correo)
-            usuariosGoogle[dni]=direcciones
-            cuentasGoogle.write(correo)
-            cuentasGoogle.write('\n')
-            cantUsuarios +=1
+def escribirCG(correo):
+    cuentasGoogle.write(correo)
+    cuentasGoogle.write('\n')
+    return correo
 
 
-sorted(usuariosGoogle.keys())
-pprint.pprint(usuariosGoogle)
-print(cantUsuarios)
+try:
+    for linea in leerArchivoUA:
+        dni=linea[0].replace('@econo.unlp.edu.ar','')
+        if int(linea[1]) > 20170600:
+            ultimoAcceso.add(dni)
+
+    for linea in leerU:
+        dni=linea[0]
+        correo=linea[1]
+        if dni in ultimoAcceso:
+            direcciones=usuariosGoogle.get(dni, [])
+            if 'econo.unlp.edu.ar' in correo:
+                direcciones.append(correo)
+                usuariosGoogle[dni]=direcciones
+                cantUsuarios +=1
+                # cuentasGoogle.write(correo)
+                # cuentasGoogle.write('\n')
+
+    for dni, correos in usuariosGoogle.items():
+        for correo in correos:
+            escribirCG(str(correo))
+            print (str(correo))
+
+
+finally:
+    archivoUA.close()
+    archivoUsuarios.close()
+
+# sorted(usuariosGoogle.keys())
+# pprint.pprint(usuariosGoogle)
+# print(cantUsuarios)
