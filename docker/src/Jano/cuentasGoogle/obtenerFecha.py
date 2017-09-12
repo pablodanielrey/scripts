2b81d8a12732b9c99882a5ffb4e0dda951ecffeb
Jano <oportoalejandroa@gmail.com>
2017-09-12 10:27:29 -0300

    se agrego try y finally

--- docker/src/Jano/cuentasGoogle/obtenerFecha.py
+++ docker/src/Jano/cuentasGoogle/obtenerFecha.py
@@ -6,49 +6,58 @@ import pprint
 ultimoAcceso= set()
 usuariosGoogle={}
 
-archivoUA=open('ua.csv','r')
-leerArchivoUA=csv.reader(archivoUA)
 
-archivoUsuarios=open('usuarios.csv','r')
-leerU=csv.reader(archivoUsuarios)
-
-cuentasGoogle = open("cuentasGoogle.txt", "w")
-
-cantUsuarios=0
-
-def escribirCG(correo):
-    cuentasGoogle.write(correo)
-    cuentasGoogle.write('\n')
+def escribirCG(archivo, correo):
+    archivo.write(correo)
+    archivo.write('\n')
     return correo
 
 
+archivoUA=open('ua.csv','r')
 try:
-    for linea in leerArchivoUA:
-        dni=linea[0].replace('@econo.unlp.edu.ar','')
-        if int(linea[1]) > 20170600:
-            ultimoAcceso.add(dni)
+    leerArchivoUA=csv.reader(archivoUA)
+    archivoUsuarios=open('usuarios.csv','r')
+    try:
+        leerU=csv.reader(archivoUsuarios)
+        cuentasGoogle = open("cuentasGoogle.txt", "w")
+        try:
+            cantUsuarios=0
 
-    for linea in leerU:
-        dni=linea[0]
-        correo=linea[1]
-        if dni in ultimoAcceso:
-            direcciones=usuariosGoogle.get(dni, [])
-            if 'econo.unlp.edu.ar' in correo:
-                direcciones.append(correo)
-                usuariosGoogle[dni]=direcciones
-                cantUsuarios +=1
-                # cuentasGoogle.write(correo)
-                # cuentasGoogle.write('\n')
+            for linea in leerArchivoUA:
+                dni=linea[0].replace('@econo.unlp.edu.ar','')
+                if int(linea[1]) > 20170600:
+                    ultimoAcceso.add(dni)
 
-    for dni, correos in usuariosGoogle.items():
-        for correo in correos:
-            escribirCG(str(correo))
-            print (str(correo))
+            for linea in leerU:
+                dni=linea[0]
+                correo=linea[1]
+                if dni in ultimoAcceso:
+                    direcciones=usuariosGoogle.get(dni, [])
+                    if 'econo.unlp.edu.ar' in correo:
+                        direcciones.append(correo)
+                        usuariosGoogle[dni]=direcciones
+                        cantUsuarios +=1
+                        # cuentasGoogle.write(correo)
+                        # cuentasGoogle.write('\n')
 
+            for dni, correos in usuariosGoogle.items():
+                for correo in correos:
+                    escribirCG(cuentasGoogle, correo)
+                    print (correo)
+
+
+        finally:
+            cuentasGoogle.close()
+
+    finally:
+        archivoUsuarios.close()
 
 finally:
     archivoUA.close()
-    archivoUsuarios.close()
+
+
+
+
 
 # sorted(usuariosGoogle.keys())
 # pprint.pprint(usuariosGoogle)
