2c31c5eef4e09bea9dc0d51b4713b04dd5741328
Jano <oportoalejandroa@gmail.com>
2017-09-12 16:34:59 -0300

    creamos archivo de prueba de coneccion a la base

--- docker/src/Jano/base-de-datos/main.py
+++ docker/src/Jano/base-de-datos/main.py
@@ -12,9 +12,12 @@ if __name__ == '__main__':
     try:
         cur = con.cursor()
         try:
-            cur.execute('select name, lastname from profile.users limit 10')
-            for nombre, apellido in cur:
-                print(nombre + ' ' + apellido)
+            cur.execute('select id, name, lastname from profile.users order by name, lastname limit 10')
+            for row in cur:
+                id = row[0]
+                nombre = row[1]
+                apellido = row[2]
+                print(id + ' ' + nombre + ' ' + apellido)
 
         finally:
             cur.close()
