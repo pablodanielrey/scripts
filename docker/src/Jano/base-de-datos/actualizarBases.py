5821cf7412a6ad56960c0cfc7c196a01fa5f2dd9
root <root@pc7comp.econo>
2017-10-03 16:38:09 -0300

    aaaaa

--- docker/src/Jano/base-de-datos/actualizarBases.py
+++ docker/src/Jano/base-de-datos/actualizarBases.py
@@ -5,6 +5,93 @@ import psycopg2
 import psycopg2.extras
 import pprint
 import re
+import uuid
+
+
+def consutarCorreoEcono(id):
+    curcorreoEcono = conEcono.cursor()
+    try:
+        curcorreoEcono.execute("SELECT correo FROM correos WHERE usuario_id = (%s);",(id,))
+        correo = str(curcorreoEcono.fetchone())
+        direccion = re.findall(r'[\w\.-]+@[\w\.-]+', correo)
+        return direccion
+
+    finally:
+        curcorreoEcono.close()
+
+
+
+
+def consutarClaveEcono(id):
+    curclaveEcono = conEcono.cursor()
+    try:
+        curclaveEcono.execute("SELECT clave FROM claves WHERE usuario_id = (%s);",(id,))
+        clave = str(curclaveEcono.fetchone())
+        contrasena = re.findall('\w+', clave, re.IGNORECASE)
+        return contrasena
+    finally:
+        curclaveEcono.close()
+
+def obtenerUsuariosEcono(conEcono):
+    curusuarioEcono = conEcono.cursor()
+    try:
+       curusuarioEcono.execute('SELECT * FROM usuarios;')
+       usuarios = curusuarioEcono.fetchall()
+       return usuarios
+
+    finally:
+        curusuarioEcono.close()
+
+def procesarUsuariosEcono(usuarios):
+    for row in usuarios:
+        id = row[0]
+        nombre = row[1]
+        apellido = row[2]
+        dni = row[3]
+
+        direccion=consutarCorreoEcono(id)
+        contrasena=consutarClaveEcono(id)
+
+        usuarioEcono['ID']=row[0]
+        usuarioEcono['NOMBRE']=row[1]
+        usuarioEcono['APELLIDO']=row[2]
+        usuarioEcono['DNI']=row[3]
+        usuarioEcono['CORREO']=direccion[0]
+        usuarioEcono['CLAVE']=contrasena[0]
+        usuariosEcono.append(dict(usuarioEcono))
+
+    return usuariosEcono
+
+
+
+
+def consultarUsuarioGoogle(usuariosEcono):
+    curusuarioGoogle.execute('SELECT * FROM usuarios;')
+
+    # id = str(curusuarioGoogle.fetchone())
+    id = re.findall('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', str(curusuarioGoogle.fetchall()), re.IGNORECASE)
+
+    for e in usuariosEcono:
+        idEcono=e['ID']
+        print('salto')
+        for i in range(len(id)):
+            idGoogle=id[i]
+            print (idEcono +' = '+ idGoogle)
+            if idEcono == idGoogle:
+                print (idGoogle + '                    Existe en Google')
+                print ('\n')
+            else:
+                idEcono=e['ID']
+                nombreEcono=e['NOMBRE']
+                apellidoEcono=e['APELLIDO']
+                dniEcono=e['DNI']
+                print(idEcono + '                       No Existe en Google')
+                print ('\n')
+
+
+                # curusuarioGoogle.execute("INSERT INTO usuarios (id, nombre, apellido, dni) VALUES (%s, %s, %s, %s)",(idEcono, nombreEcono, apellidoEcono, dniEcono));
+                # conGoogle.commit()
+
 
 if __name__ == '__main__':
 
@@ -15,70 +102,30 @@ if __name__ == '__main__':
     userEcono = os.environ['USERE']
     passwdEcono = os.environ['PASSE']
     baseEcono = os.environ['BASEE']
-    con = psycopg2.connect(host=hostEcono, user=userEcono, password=passwdEcono, database=baseEcono)
-
-    def consutarCorreoEcono(id):
-        curcorreo.execute("SELECT correo FROM correos WHERE usuario_id = (%s);",(id,))
-        correo = str(curcorreo.fetchone())
-        direccion = re.findall(r'[\w\.-]+@[\w\.-]+', correo)
-        return direccion
-
-    def consutarClaveEcono(id):
-        curclave.execute("SELECT clave FROM claves WHERE usuario_id = (%s);",(id,))
-        clave = str(curclave.fetchone())
-        contrasena = re.findall('\w+', clave, re.IGNORECASE)
-        return contrasena
-
-    def consultarUsuarioEcono(curcorreo, curclave):
-        curusuario.execute('SELECT * FROM usuarios;')
-        for row in curusuario:
-            id = row[0]
-            nombre = row[1]
-            apellido = row[2]
-            dni = row[3]
-
-            curcorreo = con.cursor()
-            try:
-                direccion=consutarCorreoEcono(id)
-            finally:
-                curcorreo.close()
-
-            curclave = con.cursor()
-            try:
-                contrasena=consutarClaveEcono(id)
-            finally:
-                curclave.close()
-
-            usuarioEcono['ID']=row[0]
-            usuarioEcono['NOMBRE']=row[1]
-            usuarioEcono['APELLIDO']=row[2]
-            usuarioEcono['DNI']=row[3]
-            usuarioEcono['CORREO']=direccion[0]
-            usuarioEcono['CLAVE']=contrasena[0]
-            usuariosEcono.append(dict(usuarioEcono))
 
     hostGoogle = os.environ['HOSTG']
     userGoogle = os.environ['USERG']
     passwdGoogle = os.environ['PASSG']
     baseGoogle = os.environ['BASEG']
-    conGoogle = psycopg2.connect(host=hostGoogle, user=userGoogle, password=passwdGoogle, database=baseGoogle)
 
-    def consultarUsuarioGoogle(usuariosEcono):
-        for usuario in usuariosEcono:
-            print(usuario.items())
-            print ('*************************')
+
+    conEcono = psycopg2.connect(host=hostEcono, user=userEcono, password=passwdEcono, database=baseEcono)
 
     try:
-        curcorreo = con.cursor()
-        curclave = con.cursor()
-        curusuario = con.cursor()
-
+        conGoogle = psycopg2.connect(host=hostGoogle, user=userGoogle, password=passwdGoogle, database=baseGoogle)
         try:
-            consultarUsuarioEcono(curcorreo, curclave)
-            consultarUsuarioGoogle(usuariosEcono)
 
+            curusuarioGoogle = conGoogle.cursor()
+
+            try:
+                usuarios = obtenerUsuariosEcono(conEcono)
+                usuariosEcono = procesarUsuariosEcono(usuarios)
+                consultarUsuarioGoogle(usuariosEcono)
+
+            finally:
+                curusuarioGoogle.close()
         finally:
-            curusuario.close()
+            conGoogle.close()
 
     finally:
-        con.close()
+        conEcono.close()
--- docker/src/Jano/base-de-datos/main.py
+++ docker/src/Jano/base-de-datos/main.py
@@ -23,39 +23,36 @@ if __name__ == '__main__':
         curusuario = con.cursor()
 
         try:
-        #     sql = "INSERT INTO correos (id, usuario_id, correo) VALUES ('{}', 'b44a70cb-19fa-4134-923b-fd4d39d13abd', 'paul@econo.unlp.edu.ar' )".format(id)
-        #     print(sql)
-        #     cur.execute(sql);
-        #     con.commit()
-        #     for row in cur:
-        #         id = row[0]
-        #         nombre = row[1]
-        #         print(id + ' ' + nombre)
-        # except Exception as e:
-        #     print (e)
+            # sql = ("INSERT INTO correos (id, usuario_id, correo) VALUES ((%s), 'b44a70cb-19fa-4134-923b-fd4d39d13abd', 'paul@econo.unlp.edu.ar' )",(id, ))
+            curusuario.execute('''INSERT INTO usuarios VALUES ('b44a70cb-19fa-4134-923b-fd4d39d13abd', 'Paul', 'California', 30000000)''');
+            con.commit()
+            # for row in curusuario:
+            #     id = row[0]
+            #     nombre = row[1]
+            #     print(id + ' ' + nombre)
+        except Exception as e:
+            print (e)
 
 
-            curusuario.execute('SELECT * FROM usuarios;')
-            for row in curusuario:
-                id = row[0]
-                nombre = row[1]
-                apellido = row[2]
-                dni = row[3]
-
-
-                    curcorreo.execute("SELECT correo FROM correos WHERE usuario_id = '{}';".format(id))
-                    correo = str(curcorreo.fetchone())
-                finally:
-                    curcorreo.close()
-
-                curclave = con.cursor()
-                try:
-                    curclave.execute("SELECT clave FROM claves WHERE usuario_id = '{}';".format(id))
-                    clave= str(curclave.fetchone())
-                finally:
-                    curclave.close()
-
-                print(id + ' | ' + nombre + ' | ' + apellido + ' | ' + dni + ' | ' + correo + ' | ' + clave)
+            # curusuario.execute('SELECT * FROM usuarios;')
+            # for row in curusuario:
+            #     id = row[0]
+            #     nombre = row[1]
+            #     apellido = row[2]
+            #     dni = row[3]
+            #
+            #     curcorreo = con.cursor()
+            #     curcorreo.execute("SELECT correo FROM correos WHERE usuario_id = (%s);",(id,))
+            #     correo = str(curcorreo.fetchone())
+            #     curcorreo.close()
+            #
+            #     curclave = con.cursor()
+            #     curclave.execute("SELECT clave FROM claves WHERE usuario_id = (%s);",(id,))
+            #     clave= str(curclave.fetchone())
+            #
+            #     curclave.close()
+            #
+            #     print(id + ' | ' + nombre + ' | ' + apellido + ' | ' + dni + ' | ' + correo + ' | ' + clave)
 
 
         finally:
