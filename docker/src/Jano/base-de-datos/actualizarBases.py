3fd03b22c2b41ada536add1809e752b76d7ae028
Jano <oportoalejandroa@gmail.com>
2017-10-05 17:44:38 -0300

    el script ya actualizada los datos de usuarios de una base a otra de acuerdo a la fecha mas reciente de modificacion de sus datos

--- docker/src/Jano/base-de-datos/actualizarBases.py
+++ docker/src/Jano/base-de-datos/actualizarBases.py
@@ -8,27 +8,34 @@ import re
 import uuid
 
 
-def consutarCorreoEcono(id):
+def consultarCorreoEcono(id):
     curcorreoEcono = conEcono.cursor()
     try:
         curcorreoEcono.execute("SELECT correo FROM correos WHERE usuario_id = (%s);",(id,))
         correo = str(curcorreoEcono.fetchone())
         direccion = re.findall(r'[\w\.-]+@[\w\.-]+', correo)
+
         return direccion
 
     finally:
         curcorreoEcono.close()
 
-
-
-
-def consutarClaveEcono(id):
+def consultarClaveEcono(id):
     curclaveEcono = conEcono.cursor()
     try:
-        curclaveEcono.execute("SELECT clave FROM claves WHERE usuario_id = (%s);",(id,))
-        clave = str(curclaveEcono.fetchone())
-        contrasena = re.findall('\w+', clave, re.IGNORECASE)
-        return contrasena
+        curclaveEcono.execute("SELECT usuario_id, clave, modificado FROM claves WHERE usuario_id = (%s);",(id,))
+        row = curclaveEcono.fetchone()
+        usuario_id=row[0]
+        clave = row[1]
+        fechaModClave=row[2]
+
+        claveEcono['USUARIO_ID']=usuario_id
+        claveEcono['CLAVE']=clave
+        claveEcono['MODIFICADO']=fechaModClave
+        clavesEcono.append(dict(claveEcono))
+
+
+        return clavesEcono
     finally:
         curclaveEcono.close()
 
@@ -36,8 +43,8 @@ def obtenerUsuariosEcono(conEcono):
     curusuarioEcono = conEcono.cursor()
     try:
        curusuarioEcono.execute('SELECT * FROM usuarios;')
-       usuarios = curusuarioEcono.fetchall()
-       return usuarios
+       usuariosE = curusuarioEcono.fetchall()
+       return usuariosE
 
     finally:
         curusuarioEcono.close()
@@ -48,55 +55,113 @@ def procesarUsuariosEcono(usuarios):
         nombre = row[1]
         apellido = row[2]
         dni = row[3]
+        creado = row[4]
+        modificado = row[5]
+        eliminado = row[6]
 
-        direccion=consutarCorreoEcono(id)
-        contrasena=consutarClaveEcono(id)
+        direccion=consultarCorreoEcono(id)
+        contrasena=consultarClaveEcono(id)
 
-        usuarioEcono['ID']=row[0]
-        usuarioEcono['NOMBRE']=row[1]
-        usuarioEcono['APELLIDO']=row[2]
-        usuarioEcono['DNI']=row[3]
+        usuarioEcono['ID']=id
+        usuarioEcono['NOMBRE']=nombre
+        usuarioEcono['APELLIDO']=apellido
+        usuarioEcono['DNI']=dni
+        usuarioEcono['CREADO']=creado
+        usuarioEcono['MODIFICADO']=modificado
+        usuarioEcono['ELIMINADO']=eliminado
         usuarioEcono['CORREO']=direccion[0]
-        usuarioEcono['CLAVE']=contrasena[0]
+        usuarioEcono['CLAVE']=contrasena[-1]['CLAVE']
         usuariosEcono.append(dict(usuarioEcono))
 
     return usuariosEcono
 
+def consultarClaveGoogle(id):
+    curclaveGoogle = conGoogle.cursor()
+    try:
+        curclaveGoogle.execute("SELECT usuario_id, clave, modificado FROM claves WHERE usuario_id = (%s);",(id,))
+        row = curclaveGoogle.fetchone()
+        usuario_id=row[0]
+        clave = row[1]
+        fechaModClave=row[2]
+
+        claveGoogle['USUARIO_ID']=usuario_id
+        claveGoogle['CLAVE']=clave
+        claveGoogle['MODIFICADO']=fechaModClave
+        clavesGoogle.append(dict(claveGoogle))
+
+        return clavesGoogle
+
+    finally:
+        curclaveGoogle.close()
 
 
 
-def consultarUsuarioGoogle(usuariosEcono):
-    curusuarioGoogle.execute('SELECT * FROM usuarios;')
+def actualizarUsuariosGoogle(cur, usuariosEcono):
 
-    # id = str(curusuarioGoogle.fetchone())
-    id = re.findall('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', str(curusuarioGoogle.fetchall()), re.IGNORECASE)
+    cur.execute('SELECT id, modificado FROM usuarios;')
+    usuariosGoogle = {}
+    for e in [{'id':c[0],'modificado':c[1]} for c in cur]:
+        usuariosGoogle[e['id']] = e['modificado']
+
+    for u in usuariosEcono:
+        econoId = u['ID']
+        if u['MODIFICADO'] > usuariosGoogle[econoId]:
+            cur.execute("UPDATE usuarios SET modificado=current_timestamp WHERE id = (%s);",(econoId,))
+            print ('Actualizado recientemente en Google ---> ' + econoId)
+        else:
+            print ('Ya esta actualizado en Google ---> ' + econoId)
+
+
+
+
+def insertarUsuarioGoogle(cur, usuariosEcono):
+
+    cur.execute('SELECT id FROM usuarios;')
+    idsGoogle = [c[0] for c in cur]
 
     for e in usuariosEcono:
         idEcono=e['ID']
-        print('salto')
-        for i in range(len(id)):
-            idGoogle=id[i]
-            print (idEcono +' = '+ idGoogle)
-            if idEcono == idGoogle:
-                print (idGoogle + '                    Existe en Google')
-                print ('\n')
-            else:
-                idEcono=e['ID']
-                nombreEcono=e['NOMBRE']
-                apellidoEcono=e['APELLIDO']
-                dniEcono=e['DNI']
-                print(idEcono + '                       No Existe en Google')
-                print ('\n')
+        if idEcono not in idsGoogle:
+            nombreEcono=e['NOMBRE']
+            apellidoEcono=e['APELLIDO']
+            dniEcono=e['DNI']
+            creadoEcono=e['CREADO']
+            modificadoEcono=e['MODIFICADO']
+            eliminadoEcono=e['ELIMINADO']
+            correoEcono=e['CORREO']
+            claveEcono=e['CLAVE']
 
+            idCorreoEcono=str(uuid.uuid4());
+            idClaveEcono=str(uuid.uuid4());
 
-                # curusuarioGoogle.execute("INSERT INTO usuarios (id, nombre, apellido, dni) VALUES (%s, %s, %s, %s)",(idEcono, nombreEcono, apellidoEcono, dniEcono));
-                # conGoogle.commit()
+            cur.execute("""
+               INSERT INTO usuarios (id, nombre, apellido, dni, creado, modificado, eliminado)
+               VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """,(idEcono, nombreEcono, apellidoEcono, dniEcono, creadoEcono, modificadoEcono, eliminadoEcono));
+
+            cur.execute("""
+               INSERT INTO correos (id, usuario_id, correo, creado, modificado, eliminado)
+               VALUES (%s, %s, %s, %s, %s, %s)
+            """,(idCorreoEcono, idEcono, correoEcono, creadoEcono, modificadoEcono, eliminadoEcono));
+
+            cur.execute("""
+               INSERT INTO claves (id, usuario_id, clave, creado, modificado, eliminado)
+               VALUES (%s, %s, %s, %s, %s, %s)
+            """,(idClaveEcono, idEcono, claveEcono, creadoEcono, modificadoEcono, eliminadoEcono));
+
+            print ('Agregado recientemente a Google ---> ' + idEcono)
 
 
 if __name__ == '__main__':
 
-    usuariosEcono=[]
+    claveEcono={}
+    clavesEcono=[]
     usuarioEcono = {}
+    usuariosEcono=[]
+
+    claveGoogle={}
+    clavesGoogle=[]
+
 
     hostEcono = os.environ['HOSTE']
     userEcono = os.environ['USERE']
@@ -116,11 +181,14 @@ if __name__ == '__main__':
         try:
 
             curusuarioGoogle = conGoogle.cursor()
-
             try:
-                usuarios = obtenerUsuariosEcono(conEcono)
-                usuariosEcono = procesarUsuariosEcono(usuarios)
-                consultarUsuarioGoogle(usuariosEcono)
+                usuariosE = obtenerUsuariosEcono(conEcono)
+                usuariosEcono = procesarUsuariosEcono(usuariosE)
+
+                insertarUsuarioGoogle(curusuarioGoogle, usuariosEcono)
+                actualizarUsuariosGoogle(curusuarioGoogle, usuariosEcono)
+                conGoogle.commit()
+
 
             finally:
                 curusuarioGoogle.close()
