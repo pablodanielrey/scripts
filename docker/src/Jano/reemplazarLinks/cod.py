c67a63fba47377cc87a53cf58ed97b24592cc003
Jano <oportoalejandroa@gmail.com>
2017-10-23 16:50:35 -0300

    a

--- docker/src/Jano/reemplazarLinks/cod.py
+++ docker/src/Jano/reemplazarLinks/cod.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
-import glob
-from chardet.universaldetector import UniversalDetector
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-detector = UniversalDetector()
-for filename in glob.glob('*.rdf'):
-    print (filename.ljust(60))
-    detector.reset()
-    for line in file(filename, 'rb'):
-        detector.feed(line)
-        if detector.done:
-            break
-    detector.close()
-    print (detector.result)
+# Make it work with Python 2 and Python 3
+import sys
+PY3 = sys.version > '3'
+
+if not PY3:
+    from future.builtins import open
+
+# Specify the encoding while opening it
+with open("cedlas0003.rdf", encoding="ISO-8859-1") as f:
+    content = f.read()
+content = content.encode('UTF-8', 'replace')
+print(content)
