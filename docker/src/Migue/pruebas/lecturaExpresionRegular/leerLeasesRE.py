0878d4530041dad2e472955d8327132d783ececc
Miguel <miguel.macagno@gmail.com>
2017-11-01 12:42:23 -0300

    Se cambio expresion regular

--- docker/src/Migue/pruebas/lecturaExpresionRegular/leerLeasesRE.py
+++ docker/src/Migue/pruebas/lecturaExpresionRegular/leerLeasesRE.py
@@ -1,9 +1,11 @@
-import re
+import re, datetime
 
 #Hay que verificar que pasa cuando no existe hostname en un lease, no corta busqueda hasta que no encuentre el del siguiente lease que tenga hostname.
-expresionLeaseCompleto=re.compile('^lease (?P<IP>[0-9\.]+) .*? starts 1 (?P<Inicio>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});.*? ends 1 (?P<Fin>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});.*?hardware ethernet (?P<MAC>([0-9A-Fa-f]{2}[:]){5}[0-9A-Fa-f]{2});.*?client-hostname "(?P<Hostname>[a-z0-9\-]+)";.*?}',re.I|re.S|re.M)
+#expresionLeaseCompleto=re.compile('^lease (?P<IP>[0-9\.]+) .*? starts 1 (?P<Inicio>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});.*? ends 1 (?P<Fin>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});.*?hardware ethernet (?P<MAC>([0-9A-Fa-f]{2}[:]){5}[0-9A-Fa-f]{2});.*?client-hostname "(?P<Hostname>[a-z0-9\-]+)";.*?}',re.I|re.S|re.M)
+#lease (?P<ip>\d+\.\d+\.\d+\.\d+) {\s* starts 1 (?P<inicio>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});\s* ends 1 (?P<fin>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});\s*(?P<config>[\s\S]+?)\n}
+expresionLeaseCompleto=re.compile(r"lease (?P<ip>\d+\.\d+\.\d+\.\d+) {\s* starts 1 (?P<inicio>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});\s* ends 1 (?P<fin>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});\s*(?P<config>[\s\S]+?)\n}")
 
-expresionMAC = re.compile('54:27:58:d7:27:73')
+expresionMAC = re.compile('18:21:95:97:9c:9d')
 
 def lectura():
     try:
@@ -13,23 +15,20 @@ def lectura():
         archivo.close()
     return ar
 
+def busqueda(iterador):
+    resultado={'ip':'0.0.0.0','inicio':'1970-01-01 00:00:00','fin':'1970-01-01 00:00:00','datos':'0'}
+    fechaUltimo=datetime.datetime(1970,1,1,0,0,0)
+    for item in iterador:
+        if expresionMAC.search(item.group('config')) != None:
+            if fechaUltimo<item.group('inicio'):
+                fechaUltimo=item.group('inicio')
+                resultado['ip']=item.group('ip')
+                resultado['inicio']=item.group('inicio')
+                resultado['fin']=item.group('fin')
+                resultado['datos']=item.group('config')
+    return resultado
+
 if __name__ == "__main__":
     archivo=lectura()
     iterador=expresionLeaseCompleto.finditer(archivo)
-    for item in iterador:
-        '''print('------------------------------------')
-        print('IP:',item.group('IP'))
-        print('Inicio:',item.group('Inicio'))
-        print('Fin:',item.group('Fin'))
-        print('MAC:',item.group('MAC'))
-        print('HOSTNAME:',item.group('Hostname'))
-        '''
-        if expresionMAC.search(item.group('MAC')) == None:
-            print('No es.')
-        else:
-            print('IP:',item.group('IP'))
-            print('Inicio:',item.group('Inicio'))
-            print('Fin:',item.group('Fin'))
-            print('MAC:',item.group('MAC'))
-            print('HOSTNAME',item.group('Hostname'))
-            break
+    print (resultado=busqueda(iterador))
