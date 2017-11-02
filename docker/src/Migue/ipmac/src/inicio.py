614f69c5683f8dfda41e04e778b086b81b0a8414
Miguel <miguel.macagno@gmail.com>
2017-11-02 12:54:14 -0300

    funciona, queda verificar y mejorar el codigo.

--- docker/src/Migue/ipmac/src/inicio.py
+++ docker/src/Migue/ipmac/src/inicio.py
@@ -1,21 +1,36 @@
 from flask import Flask, render_template, jsonify
-import re
+import re, datetime
 
 #Crea instancia de aplicacion
 app = Flask(__name__)
 expresionMAC = re.compile('^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$')
+expresionLeaseCompleto = re.compile(r"lease (?P<ip>\d+\.\d+\.\d+\.\d+) {\s* starts 1 (?P<inicio>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});\s* ends 1 (?P<fin>[\d]{4}/[\d]{2}/[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2});\s*(?P<config>[\s\S]+?)\n}")
 
-######################## Prueba de almacenamiento ##############################
-leases= [
-    {
-        'mac': 'ff:ff:ff:ff:ff:ff',
-        'ip': '127.0.0.1'
-    },
-    {
-        'mac': 'ab:ab:ab:ab:ab:ab',
-        'ip': '192.168.0.1'
-    }
-]
+######################## Acceso a datos ##############################
+def lectura():
+    try:
+        archivo=open('dhcpd.leases','r')
+        ar=archivo.read()
+    finally:
+        archivo.close()
+    return ar
+
+def busqueda(iterador,mac):
+    resultado = {}
+    expresionMACBuscada= re.compile(mac)
+    fechaUltimo=datetime.datetime(1970,1,1,0,0,0)
+    for item in iterador:
+        if expresionMACBuscada.search(item.group('config')):
+            fechaActual=datetime.datetime.strptime(item.group('inicio'), "%Y/%m/%d %H:%M:%S")
+            print(fechaActual)
+            if fechaUltimo<fechaActual:
+                fechaUltimo=fechaActual
+                resultado['ip']=item.group('ip')
+                resultado['inicio']=item.group('inicio')
+                resultado['fin']=item.group('fin')
+                resultado['datos']=item.group('config')
+    return resultado
+
 ######################## Ruta por defecto / ####################################
 @app.route("/")
 def index():
@@ -34,24 +49,15 @@ def ipreturn_vacio():
 
 @app.route('/ipmac/api/v1.0/<mac>', methods=['GET'])
 def ipreturn(mac):
-    if expresionMAC.search(mac)==None:
+    if not expresionMAC.search(mac):
         return error('Direccion MAC Invalida')
-    '''
-    try:
-        leases=open('/var/lib/dhcpd/dhcpd.leases','r')
-
-    except:
-        return error('Error de acceso al archivo leases')
-    finally:
-        leases.close()
-    '''
-
-    #Busqueda de asignaciones en variable leases ¡¡¡Falta Implementar busqeuda en leases!!!
-    asignacion=[asignacion for asignacion in leases if asignacion['mac'].lower()==mac.lower()]
-    #Pregunto si existe dato alguno adentro de asignacion
-    if len(asignacion)==0:
-        return error('No existe registro en el leases para esa direccion MAC')
-    return jsonify({'asignacion':asignacion[0]})
+    else:
+        archivo=lectura()
+        resultado=busqueda(expresionLeaseCompleto.finditer(archivo),mac)
+        if not resultado:
+            return error('No existe registro en el leases para esa direccion MAC')
+        else:
+            return jsonify({'ip':resultado['ip']})
 
 ################################################################################
 if __name__ == "__main__":
