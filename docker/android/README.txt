5dbce43f2b8babf5759f15539b4379b96481b043
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-12-25 16:17:38 -0300

    agregue android studio para pdoer desarrollar con eso. para poder ejecutarlo deben configurar la variable DISPLAY ej: export DISPLAY=:1 ./studio.sh

---
+++ docker/android/README.txt
@@ -0,0 +1,4 @@
+descargar el ultimo android studio
+y hacer un link simbólico para que se llame android-studio.zip
+o renombarlo.
+
--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sudo docker run -ti --name scripts --env-file $HOME/gitlab/fce/produccion/scripts -p 9090:5000 -v $(pwd)/src:/src scripts bash
+sudo docker run -ti --rm --name scripts --env-file $HOME/gitlab/fce/produccion/scripts \
+	-p 9090:5000 \
+	-v $(pwd)/src:/src \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v $(pwd)/android/home:/home/developer \
+	scripts bash
 
