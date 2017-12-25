4b48f7c914c2b5d2b759be36588558e30546edbb
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-12-25 17:34:42 -0300

    agregue el path del home para poder guarlar los proyectos y que no se eliminen cuando se elimna el contenedor

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -4,5 +4,7 @@ sudo docker run -ti --rm --name scripts --env-file $HOME/gitlab/fce/produccion/s
 	-v $(pwd)/src:/src \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v $(pwd)/android/home:/home/developer \
+	--privileged \
+	-v /dev/bus/usb:/dev/bus/usb \
 	scripts bash
 
